d48e90b7b681601595d166163082377ed5460c35
DELORME Loïc <loic.delorme.pro@gmail.com>
2016-03-31 08:47:12 +0200

    Add the color enumeration

---
+++ src/fr/polytech/reversi/model/game/Color.java
@@ -0,0 +1,20 @@
+package fr.polytech.reversi.model.game;
+
+/**
+ * This enumeration represents the player color (White or Black).
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public enum Color
+{
+    /**
+     * The white color.
+     */
+	WHITE,
+
+	/**
+	 * The black color.
+	 */
+	BLACK;
+}
