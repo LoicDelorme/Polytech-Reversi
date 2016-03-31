7ac902b8ee6b515e64e5d0716b52af01237b674e
DELORME Loïc <loic.delorme.pro@gmail.com>
2016-03-31 08:40:04 +0200

    Add player type enumeration

---
+++ src/fr/polytech/reversi/model/players/PlayerType.java
@@ -0,0 +1,20 @@
+package fr.polytech.reversi.model.players;
+
+/**
+ * This enumeration represents the player type (Human or Computer)
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public enum PlayerType
+{
+    /**
+     * The player is a human.
+     */
+	HUMAN,
+
+	/**
+	 * The player is a computer.
+	 */
+	COMPUTER;
+}
