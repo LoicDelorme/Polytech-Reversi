0fc85ba30566c6e7256483ad716d243a40954004
DELORME Loïc <loic.delorme.pro@gmail.com>
2016-03-31 08:52:29 +0200

    Add human player implementation

---
+++ src/fr/polytech/reversi/model/players/HumanPlayer.java
@@ -0,0 +1,57 @@
+package fr.polytech.reversi.model.players;
+
+import fr.polytech.reversi.model.boardgame.Position;
+import fr.polytech.reversi.model.game.Color;
+
+/**
+ * This class represents a human player.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class HumanPlayer implements IPlayer
+{
+	/**
+	 * The player color.
+	 */
+	private final Color color;
+
+	/**
+	 * Create a human player.
+	 * 
+	 * @param color
+	 *            The human color.
+	 */
+	public HumanPlayer(Color color)
+	{
+		this.color = color;
+	}
+
+	/**
+	 * @see fr.polytech.reversi.model.players.IPlayer#getNextChoice()
+	 */
+	@Override
+	public Position getNextChoice()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see fr.polytech.reversi.model.players.IPlayer#getPlayerType()
+	 */
+	@Override
+	public PlayerType getPlayerType()
+	{
+		return PlayerType.HUMAN;
+	}
+
+	/**
+	 * @see fr.polytech.reversi.model.players.IPlayer#getPlayerColor()
+	 */
+	@Override
+	public Color getPlayerColor()
+	{
+		return this.color;
+	}
+}
